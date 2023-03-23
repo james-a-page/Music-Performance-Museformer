0fa77afcf4424552c93f20fc1129307ccf355d4c
James Page <james.a.page@warwick.ac.uk>
2023-03-23 23:41:59 +0000

    Museformer mask start point

--- train/models/model/transformer.py
+++ train/models/model/transformer.py
@@ -9,6 +9,8 @@ from torch import nn
 from models.model.decoder import Decoder
 from models.model.encoder import Encoder
 
+from models.mask_utils import *
+
 
 class Transformer(nn.Module):
 
@@ -67,3 +69,33 @@ class Transformer(nn.Module):
         mask = torch.tril(torch.ones(len_q, len_k)).type(torch.BoolTensor).to(self.device)
 
         return mask
+
+    def make_museformer_mask(self, q, k, q_bar_idx, k_bar_idx, struct_related_bars=[1,2,4,8,16,32], use_eos_as_summary=True):
+        # Generate the mask for the seqeuence based on https://arxiv.org/pdf/2210.10349.pdf
+        # Choice between calculating structure related bars based on similarity or using those suggested from the paper.
+
+        # First split sequence into bars by the bar token
+        q_bar_pointers = get_bar_locations(q, q_bar_idx)
+        k_bar_pointers = get_bar_locations(k, k_bar_idx)
+
+        """
+        General masking scheme example where the 1st bar (x1-3) is a structure related bar for bar 3 (x6-8)
+
+            | b1 x1 x2 x3 b2 x4 x5 b3 x6 x7 x8 b4
+        Main mask
+        x1  |    #
+        x2  |    #  #
+        x3  |    #  #  #  
+        x4  |    #  #  #     #
+        x5  |    #  #  #     #  #
+        x6  |             #  #  #     #
+        x7  |             #  #  #     #  #
+        x8  |             #  #  #     #  #  #
+        Summarisation Step
+        b1  |
+        b2  |    #  #  #  #
+        b3  |                #  #  #
+        b4  |                         #  #  #  #
+
+        (first bar gets effectively ignored, and we would treat the EOS token as a end of bar if no final bar token exists)
+        """
