a57e028debdf66b1ff4df031383c348a9c9a9909
James Page <james.a.page@warwick.ac.uk>
2023-04-02 17:48:54 +0100

    Museformer Mask Implemented

--- train/models/mask_utils.py
+++ train/models/mask_utils.py
@@ -1,17 +1,12 @@
 from typing import List, Tuple
 
-"""
 
-"""
 def get_bar_locations(sequence : List[int], summary_idx: List[int]) -> List[Tuple]:
     bar_pairs = []
-    if not sequence:
-        return bar_pairs
     bar_start = None
-    for pos, token in enumerate(sequence):
+    for pos, token in enumerate(sequence[0]):
         if token in summary_idx:
             if bar_start is not None:
                 bar_pairs.append((bar_start, pos))
             bar_start = pos + 1
-
     return bar_pairs
--- train/models/model/transformer.py
+++ train/models/model/transformer.py
@@ -11,34 +11,42 @@ from models.model.encoder import Encoder
 
 from models.mask_utils import *
 
+import torchshow as ts
 
 class Transformer(nn.Module):
-    def __init__(self, cfg, vocab_size, SOS_IDX, PAD_IDX, device, attention_method = 'museformer'):
+    def __init__(
+        self, cfg, vocab_size, SOS_IDX, PAD_IDX, device, attention_method="museformer"
+    ):
         super().__init__()
         self.src_pad_idx = PAD_IDX
         self.trg_pad_idx = PAD_IDX
         self.trg_sos_idx = SOS_IDX
         self.vocab_size = vocab_size
         self.device = device
-        self.encoder = Encoder(d_model=cfg.get("d_model"),
-                               n_head=cfg.get("n_head"),
-                               max_len=cfg.get("max_len"),
-                               ffn_hidden=cfg.get("ffn_hidden"),
-                               enc_voc_size=vocab_size,
-                               drop_prob=cfg.get("drop_prob"),
-                               n_layers=cfg.get("n_layers"),
-                               bayes_compression=cfg.get("bayes_compression"),
-                               device=device)
+        self.attention_method = attention_method
+        self.encoder = Encoder(
+            d_model=cfg.get("d_model"),
+            n_head=cfg.get("n_head"),
+            max_len=cfg.get("max_len"),
+            ffn_hidden=cfg.get("ffn_hidden"),
+            enc_voc_size=vocab_size,
+            drop_prob=cfg.get("drop_prob"),
+            n_layers=cfg.get("n_layers"),
+            bayes_compression=cfg.get("bayes_compression"),
+            device=device,
+        )
 
-        self.decoder = Decoder(d_model=cfg.get("d_model"),
-                               n_head=cfg.get("n_head"),
-                               max_len=cfg.get("max_len"),
-                               ffn_hidden=cfg.get("ffn_hidden"),
-                               dec_voc_size=vocab_size,
-                               drop_prob=cfg.get("drop_prob"),
-                               n_layers=cfg.get("n_layers"),
-                               bayes_compression=cfg.get("bayes_compression"),
-                               device=device)
+        self.decoder = Decoder(
+            d_model=cfg.get("d_model"),
+            n_head=cfg.get("n_head"),
+            max_len=cfg.get("max_len"),
+            ffn_hidden=cfg.get("ffn_hidden"),
+            dec_voc_size=vocab_size,
+            drop_prob=cfg.get("drop_prob"),
+            n_layers=cfg.get("n_layers"),
+            bayes_compression=cfg.get("bayes_compression"),
+            device=device,
+        )
 
         # Components including kl_divergence
         self.kl_list = [self.encoder, self.decoder]
@@ -46,10 +54,18 @@ class Transformer(nn.Module):
     def forward(self, src, trg):
         src_mask = self.make_pad_mask(src, src, self.src_pad_idx, self.src_pad_idx)
         src_trg_mask = self.make_pad_mask(trg, src, self.trg_pad_idx, self.src_pad_idx)
-        
 
-        trg_mask = self.make_pad_mask(trg, trg, self.trg_pad_idx, self.trg_pad_idx) * \
-                   self.make_no_peak_mask(trg, trg)
+        if self.attention_method == "museformer":
+            trg_mask = self.make_pad_mask(
+                trg, trg, self.trg_pad_idx, self.trg_pad_idx
+            ) * self.make_museformer_mask(trg, trg, 5, 5, use_eos_as_summary=False)
+
+            print(trg_mask)
+            raise SyntaxError("Test")
+        else:
+            trg_mask = self.make_pad_mask(
+                trg, trg, self.trg_pad_idx, self.trg_pad_idx
+            ) * self.make_no_peak_mask(trg, trg)
 
         enc_src = self.encoder(src, src_mask)
         output = self.decoder(trg, enc_src, trg_mask, src_trg_mask)
@@ -81,37 +97,38 @@ class Transformer(nn.Module):
 
         return mask
 
-     
+
     """
     Generate the mask for the seqeuence based on https://arxiv.org/pdf/2210.10349.pdf
 
     General masking scheme example where the 1st bar (x1-3) is a structure related bar for bar 3 (x6-8)
 
-            | b1 x1 x2 x3 b2 x4 x5 b3 x6 x7 x8 b4
+        | b1 x1 x2 x3 b2 x4 x5 b3 x6 x7 x8 b4
     Main mask
+    b1  |
     x1  |    #
     x2  |    #  #
     x3  |    #  #  #
     x4  |    #  #  #     #
     x5  |    #  #  #     #  #
+    b2  |    #  #  #  #
+    b3  |                #  #  #
     x6  |             #  #  #     #
     x7  |             #  #  #     #  #
     x8  |             #  #  #     #  #  #
-    Summarisation Step
-    b1  |
-    b2  |    #  #  #  #
-    b3  |                #  #  #
     b4  |                         #  #  #  #
+
 
     (first bar gets effectively ignored, and we would treat the EOS token as a end of bar if no final bar token exists)
     """
+
     def make_museformer_mask(
         self,
         q,
         k,
         q_bar_idx,
         k_bar_idx,
-        struct_related_bars=[1, 2, 4, 8, 16, 32],
+        struct_related_bars=[2, 4, 8, 16, 32],
         use_eos_as_summary=True,
         EOS_idx=3,
     ):
@@ -120,53 +137,58 @@ class Transformer(nn.Module):
         # First split sequence into bars by the bar token
         if use_eos_as_summary:
             summary_idx = [q_bar_idx, EOS_idx]
+        else:
+            summary_idx = [q_bar_idx]
         q_bar_pointers = get_bar_locations(q, summary_idx)
-        
-        summary_idx[0] = k_bar_idx
-        k_bar_pointers = get_bar_locations(k, summary_idx)
 
         tri_mask = (
             torch.tril(torch.ones(len_q, len_k)).type(torch.BoolTensor).to(self.device)
         )
 
-        museformer_mask = torch.zeros_like(tri_mask)
+        museformer_mask = torch.zeros_like(tri_mask).to(self.device)
 
-        bar_count = 0
+        bar_count = -1
+
+        first_of_bar = False
+
         for i, mask in enumerate(tri_mask):
-            if q[i] == q_bar_idx or (use_eos_as_summary and q[i] == EOS_idx):
-                temp_mask = torch.zeros_like(mask)
-                temp_mask[q_bar_pointers[bar_count][0] : i] = 1
-                mask = torch.logical_and(mask, temp_mask)
+            temp_mask = torch.zeros_like(mask)
+            if q[0][i] == q_bar_idx or (use_eos_as_summary and q[0][i] == EOS_idx):
+                if bar_count != -1:
+                    temp_mask.index_fill_(0, torch.arange(q_bar_pointers[bar_count][0], i).to(self.device), 1)
+                    first_of_bar = True
                 bar_count += 1
             else:
                 if bar_count > 0:
                     # unmask all but current bar and previous and then struct_related_bar tokens
                     # Previous bar
-                    temp_mask = torch.zeros_like(mask)
-                    temp_mask[q_bar_pointers[bar_count - 1][0] : i] = 1
-
-                    # Struct_related_bar
-                    for struct_bar in struct_related_bars:
-                        if bar_count > struct_bar:
-                            temp_mask[q_bar_pointers[bar_count - struct_bar]][1] = 1
-                        else:
-                            break
-                    mask = torch.logical_and(mask, temp_mask)
+                    if first_of_bar:
+                        temp_mask.index_fill_(0, torch.arange(q_bar_pointers[bar_count - 1][0], i).to(self.device), 1)
+                        temp_mask[i-1] = False
+                        # Struct_related_bar
+                        for struct_bar in struct_related_bars:
+                            if bar_count > struct_bar:
+                                temp_mask[q_bar_pointers[bar_count - struct_bar][1]] = True
+                            else:
+                                break
+                        first_of_bar = False
+                    else:
+                        temp_mask = museformer_mask[i-1]
+                        temp_mask[i] = True
                 else:
                     # unmask all but current bar
-                    # Previous bar
-                    temp_mask = torch.zeros_like(mask)
-                    temp_mask[q_bar_pointers[bar_count][0] : i] = 1
-
-                    # Struct_related_bar
-                    for struct_bar in struct_related_bars:
-                        if bar_count > struct_bar:
-                            temp_mask[q_bar_pointers[bar_count - struct_bar]][1] = 1
+                    if bar_count >= 0:
+                        if first_of_bar:    
+                            temp_mask.index_fill_(0, torch.arange(q_bar_pointers[bar_count][0], i).to(self.device), 1)
+                            temp_mask[i-1] = False
+                            first_of_bar = False
                         else:
-                            break
-                    mask = torch.logical_and(mask, temp_mask)
-            museformer_mask[i] = mask
+                            temp_mask = museformer_mask[i-1]
+                            temp_mask[i] = True
+
+            museformer_mask[i] = temp_mask
         return museformer_mask
+
     def _kl_divergence(self):
         KLD = 0
         for layer in self.kl_list:
